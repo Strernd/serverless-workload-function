66bcb0f485e13f174d305b868f9fa2c8fcc27496
Bernd Strehl <bernd.strehl@gmail.com>
2019-02-23 15:28:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+
+const hashWorkload = async (config) => {
+    const start = + new Date();
+    const plainText = config.plainText || 'benchmark';
+    const rounds = config.rounds || 10;
+    const hash = await bcrypt.hash(plainText, rounds);
+    const end = + new Date();
+    const time = end - start;
+    return {
+        start,
+        end,
+        time,
+        plainText,
+        rounds,
+        hash
+    }
+}
+
+const timeout = (min, max) => new Promise(resolve => {
+    const duration = Math.round(Math.random() * (max - min) + min);
+    setTimeout(() => resolve(duration), duration);
+})
+
+const apiWorkload = async (config) => {
+    const start = + new Date();
+    const min = config.min || 30;
+    const max = config.max || 80;
+    const apiResponseTime = await timeout(min, max);
+    const end = + new Date();
+    const time = end - start;
+    return {
+        start,
+        end,
+        time,
+        min,
+        max,
+        apiResponseTime
+    }
+}
+
+module.exports = async (type = "hash", config = {}) => {
+    if (type === 'hash') return hashWorkload(config);
+    else if (type === 'api') return apiWorkload(config);
+    else return 'unknown type'
+}
